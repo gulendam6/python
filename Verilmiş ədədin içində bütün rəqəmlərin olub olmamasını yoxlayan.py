2843250ae06505b83d44e502efb447602b1f95be
gulendam6 <58719973+gulendam6@users.noreply.github.com>
2020-01-24 13:30:24 +0400

    Create  Verilmiş ədədin içində bütün rəqəmlərin olub olmamasını yoxlayan.py

---
+++ Verilmiş ədədin içində bütün rəqəmlərin olub olmamasını yoxlayan.py
@@ -0,0 +1,11 @@
+def eded(a):
+    a = str(a)
+    li = []
+    for i in a:
+        li.append(i)
+    for y in range(0,10):
+        y = str(y)
+        if li.count(y) == 0:
+            print( y+ " Yoxdur")
+
+eded(98305614820698024492)
