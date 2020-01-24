57912ec9420856c7b54f089ae8dc85ee481825ff
gulendam6 <58719973+gulendam6@users.noreply.github.com>
2020-01-24 13:26:00 +0400

    Create Əgər aylıq maaş 1000 manatdan kiçik bərabər olarsa, 14% vergi, 2% Sosial müdafiə fonduna veriləcək. Əgər 2500 manatdan böyük bərabər olarsa 20% vergi 5% Sosial müdafiə fonduna veriləcək. Yazılan maaşdan yekun olaraq nə qədər məbləğ qalacağı.py

---
+++ Əgər aylıq maaş 1000 manatdan kiçik bərabər olarsa, 14% vergi, 2% Sosial müdafiə fonduna veriləcək. Əgər 2500 manatdan böyük bərabər olarsa 20% vergi 5% Sosial müdafiə fonduna veriləcək. Yazılan maaşdan yekun olaraq nə qədər məbləğ qalacağı.py
@@ -0,0 +1,22 @@
+def maas(a):
+    b = 0
+    c = 0
+    d = 0
+    if a <= 1000:
+        b = a * 0.14
+        c = a * 0.02
+        d = a - b - c 
+        print(d)
+        # və ya print(int(d))
+    
+    elif a >= 2500:
+        b = a * 0.20
+        c = a * 0.05
+        d = a - b - c 
+        print(d)
+        # və ya print(int(d))
+    
+    else:
+        print("Səhv")
+        
+maas(1000)
