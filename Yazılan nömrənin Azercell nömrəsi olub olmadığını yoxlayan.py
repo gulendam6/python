dca39adb0e62670182866ac5ff2aac4214f47324
gulendam6 <58719973+gulendam6@users.noreply.github.com>
2020-01-24 13:25:01 +0400

    Create Yazılan nömrənin Azercell nömrəsi olub olmadığını yoxlayan.py

---
+++ Yazılan nömrənin Azercell nömrəsi olub olmadığını yoxlayan.py
@@ -0,0 +1,12 @@
+def azc(a):
+    a = str(a)
+    if a.startswith("+99450") or a.startswith("99450"):
+        print("True")
+    elif a.startswith("+99451") or a.startswith("99451"):
+        print("True")
+    else:
+        print("False")
+    
+azc("+994513665229")
+
+#Nömrənin uzunluğunu yoxlamadım.
