6e2a41e7703061de472a5846b8c9c5d6717c541a
gulendam6 <58719973+gulendam6@users.noreply.github.com>
2020-01-24 17:20:47 +0400

    Create sozde tekrarlanan herf silmek.py

---
+++ sozde tekrarlanan herf silmek.py
@@ -0,0 +1,11 @@
+ints_list = ("iiimttttttahaaan")
+
+temp = []
+
+for x in ints_list:
+    if x not in temp:
+        temp.append(x)
+
+ints_list = temp
+
+print('Updated List after removing duplicates = {imtahan}')
